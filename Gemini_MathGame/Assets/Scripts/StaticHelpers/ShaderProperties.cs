1fb59993b0134c09c71de439f972e3794dd55096
kobeja <84107545+kobeja@users.noreply.github.com>
2024-06-24 20:26:13 -0400

    deleted some shader fluff
    also did some work on the shader for the bubble, I'm not sure if the shader is the right approach because when they all share the same material it doesn't look great but also 40 different materials may not be ideal

---
+++ Gemini_MathGame/Assets/Scripts/StaticHelpers/ShaderProperties.cs
@@ -0,0 +1,7 @@
+public static class ShaderProperties
+{
+    public const string RainbowSpeed = "_RainbowSpeed";
+    public const string RainbowBrightness = "_RainbowBrightness";
+    public const string RainbowDensity = "_RainbowDensity";
+    public const string RainbowCenter = "_RainbowCenter";
+}
--- Gemini_MathGame/Assets/Scripts/UI/Controllers/NumberBubble.cs
+++ Gemini_MathGame/Assets/Scripts/UI/Controllers/NumberBubble.cs
@@ -5,6 +5,7 @@ using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class NumberBubble : MonoBehaviour
 {
@@ -14,6 +15,11 @@ public class NumberBubble : MonoBehaviour
 
     [SerializeField] private Sprite[] _spriteSheet;
     [SerializeField] private Color _baseColor;
+    [SerializeField] private Color _baseColorIfShader;
+
+    [SerializeField] private Material _mat;
+
+    private bool _isShader => _mat != null;
     
     public TextMeshProUGUI Text => _text;
     public Image Image => _image;
@@ -25,6 +31,8 @@ public class NumberBubble : MonoBehaviour
 
     private Vector3 _startingPosition;
 
+    #region Unity Lifecycle
+
     private void Awake()
     {
         _button.onClick.AddListener(OnClick);
@@ -34,6 +42,69 @@ public class NumberBubble : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
+    private void OnEnable()
+    {
+        ResetBubbleColor();
+        RandomizeShaderProperties();
+    }
+
+    #endregion
+
+    #region Shader
+
+    private static readonly int RainbowBrightness = Shader.PropertyToID(ShaderProperties.RainbowBrightness);
+    private static readonly int RainbowSpeed = Shader.PropertyToID(ShaderProperties.RainbowSpeed);
+    private static readonly int RainbowDensity = Shader.PropertyToID(ShaderProperties.RainbowDensity);
+    private static readonly int RainbowCenter = Shader.PropertyToID(ShaderProperties.RainbowCenter);
+    private void SetRainbowBrightness(float value)
+    {
+        if (_mat != null)
+        {
+            _mat.SetFloat(RainbowBrightness, value);
+        }
+    }
+
+    private void SetRainbowSpeed(float value)
+    {
+        if (_mat != null)
+        {
+            _mat.SetFloat(RainbowSpeed, value);
+        }
+    }
+
+    private void SetRainbowDensity(float value)
+    {
+        if (_mat != null)
+        {
+            _mat.SetFloat(RainbowDensity, value);
+        }
+    }
+
+    private void SetRainbowCenter(Vector4 value)
+    {
+        if (_mat != null)
+        {
+            _mat.SetVector(RainbowCenter, value);
+        }
+    }
+
+    private void RandomizeShaderProperties()
+    {
+        if (_mat == null) return;
+        var brightness = Random.Range(1.5f, 2f);
+        var speed = Random.Range(0.03f, 0.05f);
+        var density = Random.Range(0.4f, 0.7f);
+        var centerX = Random.Range(0f, 3f);
+        var centerY = Random.Range(0f, 3f);
+
+        SetRainbowBrightness(brightness);
+        SetRainbowDensity(density);
+        SetRainbowSpeed(speed);
+        SetRainbowCenter(new Vector4(centerX, centerY));
+    }
+
+    #endregion
 
     private void OnClick()
     {
@@ -45,7 +116,11 @@ public class NumberBubble : MonoBehaviour
             ["id"] = Id,
             ["data"] = this
         };
-        _image.color = Color.yellow;
+        if (!_isShader)
+        {
+            _image.color = Color.yellow;
+        }
+
         EventManager.RaiseEvent(GameEvents.BubbleClicked, eventData);
     }
 
@@ -67,14 +142,14 @@ public class NumberBubble : MonoBehaviour
 
     public void ResetBubbleColor()
     {
-        Image.color = _baseColor;
+        Image.color = _isShader ? _baseColorIfShader : _baseColor;
     }
 
     public void ResetBubble()
     {
         Value = 0;
         State = BubbleState.NotClicked;
-        Image.color = _baseColor;
+        Image.color = _isShader ? _baseColorIfShader : _baseColor;
         _text.text = Value.ToString();
         gameObject.SetActive(false);
     }
