504b0c7cfee619cf380bfde71982f0fb2e5a3ac0
kobeja <84107545+kobeja@users.noreply.github.com>
2024-07-01 01:27:16 -0400

    hooking up more sliders in GameConfig UI
    hooked up the numbersSetLength slider and the solutionSetLength

--- Gemini_MathGame/Assets/Scripts/UI/Controllers/DifficultySelectionController.cs
+++ Gemini_MathGame/Assets/Scripts/UI/Controllers/DifficultySelectionController.cs
@@ -1,14 +1,24 @@
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class DifficultySelectionController : MonoBehaviour
 {
     [Header("Solution Target")] 
-    [SerializeField] private TextMeshProUGUI _valueDisplay;
+    [SerializeField] private TextMeshProUGUI _solutionTargetDisplay;
     [SerializeField] private Slider _solutionTargetSlider;
+    
+    [Header("Number Set")] 
+    [SerializeField] private TextMeshProUGUI _numberSetLengthDisplay;
+    [SerializeField] private Slider _numberSetLengthSlider;
+    
+    [Header("Solution Set")] 
+    [SerializeField] private TextMeshProUGUI _solutionSetLengthDisplay;
+    [SerializeField] private Slider _solutionSetLengthSlider;
 
     [Header("Control Buttons")] 
     [SerializeField] private Button _submitButton;
@@ -17,19 +27,41 @@ public class DifficultySelectionController : MonoBehaviour
     {
         _solutionTargetSlider.onValueChanged.AddListener(x =>
         {
-            _valueDisplay.text = x.ToString(CultureInfo.InvariantCulture);
+            _solutionTargetDisplay.text = x.ToString(CultureInfo.InvariantCulture);
         });
         _solutionTargetSlider.onValueChanged?.Invoke(_solutionTargetSlider.value);
         
+        _numberSetLengthSlider.onValueChanged.AddListener(OnNumberSetLengthChanged);
+        _solutionSetLengthSlider.onValueChanged.AddListener(OnSolutionSetLengthChanged);
+        
         _submitButton.onClick.AddListener(SubmitGameConfig);
+    }
+
+    private void OnNumberSetLengthChanged(float value)
+    {
+        _numberSetLengthSlider.value = RoundToValue(value, 4);
+        _numberSetLengthDisplay.text = _numberSetLengthSlider.value.ToString(CultureInfo.InvariantCulture);
+
+        _solutionSetLengthSlider.maxValue = _numberSetLengthSlider.value;
+    }
+    
+    private void OnSolutionSetLengthChanged(float value)
+    {
+        _solutionSetLengthSlider.value = RoundToValue(value, 2);
+        _solutionSetLengthDisplay.text = _solutionSetLengthSlider.value.ToString(CultureInfo.InvariantCulture);
+    }
+
+    private float RoundToValue(float sliderValue, int target)
+    {
+        return MathF.Round(sliderValue / target) * target;
     }
 
     private void SubmitGameConfig()
     {
         var config = new GameConfig
             ((int)_solutionTargetSlider.value,
-            32,
-            16,
+                (int)_numberSetLengthSlider.value,
+                (int)_solutionSetLengthSlider.value,
             EquationOperation.Addition,
             BubbleCollectionOrientation.Shuffled,
             false
